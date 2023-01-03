81eac764bf30eb8f13ce130c21cdb845b898d2a6
Hynn <choihwoong@me.com>
2023-01-03 12:47:06 +0900

    fix : added manuallly code for CORS Setup

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express()
 app.use(cors())
 
 app.get('/users', (req, res) => {
+    // res.setHeader("Access-Control-Allow-Origin", "*")
+    // res.setHeader("Access-Control-Allow-Methods","POST,GET,DELETE,OPTIONS")
+    // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Origin, X-Requested-With")
     res.send("GET /users")
 })
 
