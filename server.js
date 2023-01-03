f5d7dfd7404741cc43f021b825e32a6af5ae6a0c
Hynn <choihwoong@me.com>
2023-01-03 12:45:48 +0900

    fix : change cors setting in server.js and install cors modules

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
+
 app.get('/users', (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Methods","POST,GET,OPTIONS, DELETE")
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type")
     res.send("GET /users")
 })
 
